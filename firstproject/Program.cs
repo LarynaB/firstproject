f5d675fff437245a6f9d39025149cd824f10acd5
Twilight <lbillingh@gmail.com>
2021-11-13 22:08:24 -0800

    update

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace firstproject
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //initialize variable
+
+            //get input from user
+
+            //initialize story
+            string madlibsStory =
+            "The Matrix is a system, Neo. That system is our enemy." +
+            "But when you're inside, you look around, what do you see?" +
+            "Businessmen, teachers, lawyers, carpenters. The very minds" +
+            "of the people we are trying to save. But until we do," +
+            "these people are still a part of that system and that makes" +
+            "them our enemy. You have to understand, most of these people" +
+            "are not ready to be unplugged. And many of them are so inured," +
+            "so hopelessly dependent on the system, that they will fight to protect it.";
+
+            //print story
+            Console.WriteLine(madlibsStory);
         }
     }
 }
