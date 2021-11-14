25fd67fa6d32f75ca6f0c9666bfeff5079c56fd7
Twilight <lbillingh@gmail.com>
2021-11-13 21:41:45 -0800

    Initial commit

---
+++ firstproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstproject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
