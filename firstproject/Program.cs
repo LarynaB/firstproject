0f479788e865ef4528cb49335e957d3623e8a53c
Twilight <lbillingh@gmail.com>
2021-11-16 12:19:44 -0800

    Update Program.cs

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -11,19 +11,19 @@ namespace firstproject
         static void Main(string[] args)
         {
             //initialize variable
-
+            string theMatrix, system, neo, enemy, inside, save, fight;
             //get input from user
 
             //initialize story
             string madlibsStory =
-            "The Matrix is a system, Neo. That system is our enemy." +
-            "But when you're inside, you look around, what do you see?" +
-            "Businessmen, teachers, lawyers, carpenters. The very minds" +
-            "of the people we are trying to save. But until we do," +
-            "these people are still a part of that system and that makes" +
-            "them our enemy. You have to understand, most of these people" +
-            "are not ready to be unplugged. And many of them are so inured," +
-            "so hopelessly dependent on the system, that they will fight to protect it.";
+            $"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}." +
+            $"But when you're {inside}, you look around, what do you see?" +
+            $"Businessmen, teachers, lawyers, carpenters. The very minds" +
+            $"of the people we are trying to {save}. But until we do," +
+            $"these people are still a part of that {system} and that makes" +
+            $"them our {enemy}. You have to understand, most of these people" +
+            $"are not ready to be {unplugged}. And many of them are so inured," +
+            $"so hopelessly dependent on the {system}, that they will {fight} to protect it.";
 
             //print story
             Console.WriteLine(madlibsStory);
